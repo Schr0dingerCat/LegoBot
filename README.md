d1aeb4b5b25e9b9822ca04d0245f03869026d723
Schr0dingerCat <174683306@qq.com>
2021-09-10 20:44:29 +0800

    修改为在指定时间或随即时间运行

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 fork from https://github.com/go-acme/lego
 
-set env EVERY_X_DAYS=x, legobot can run every x days
+set env EVERY_X_DAYS=x, set env AT_TIME=HH:mm:ss, legobot can run every x days at HH:mm:ss
+if you not set them, legobot can run every 1 day at a random time
--- legobot.go
+++ legobot.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/go-acme/lego/v4/cmd"
 	"github.com/go-acme/lego/v4/log"
@@ -15,12 +18,14 @@ import (
 
 const (
 	envEveryXDays = "EVERY_X_DAYS"
+	envAtTime     = "AT_TIME"
 )
 
 var (
 	version    = "dev"
 	app        = cli.NewApp()
 	everyXDays = 1
+	atTime     = "01:02:03"
 )
 
 func init() {
@@ -58,6 +63,17 @@ func init() {
 			log.Println("env:", envEveryXDays, " is less than or equal to zero, use default value every 1 day")
 		}
 	}
+	atTime = os.Getenv(envAtTime)
+	if e == "" {
+		atTime = GetRandomTime()
+		log.Println("not set env:", envAtTime, ". use a random time:", atTime)
+	} else {
+		err = CanFormatTime(atTime)
+		if err != nil {
+			atTime = GetRandomTime()
+			log.Println("not set env:", envAtTime, ". use a random time:", atTime)
+		}
+	}
 }
 
 func task() {
@@ -68,12 +84,47 @@ func task() {
 	}
 }
 
+func CanFormatTime(t string) (err error) {
+	var hour, min, sec int
+	ts := strings.Split(t, ":")
+	if len(ts) < 2 || len(ts) > 3 {
+		return err
+	}
+
+	if hour, err = strconv.Atoi(ts[0]); err != nil {
+		return err
+	}
+	if min, err = strconv.Atoi(ts[1]); err != nil {
+		return err
+	}
+	if len(ts) == 3 {
+		if sec, err = strconv.Atoi(ts[2]); err != nil {
+			return err
+		}
+	}
+
+	if hour < 0 || hour > 23 || min < 0 || min > 59 || sec < 0 || sec > 59 {
+		return err
+	}
+
+	return nil
+}
+
+func GetRandomTime() string {
+	var h, m, s int
+	rand.Seed(time.Now().UnixNano())
+	h = rand.Intn(24)
+	m = rand.Intn(60)
+	s = rand.Intn(60)
+	return strconv.Itoa(h) + ":" + strconv.Itoa(m) + ":" + strconv.Itoa(s)
+}
+
 func main() {
 	s := gocron.NewScheduler()
 	if everyXDays == 1 {
-		s.Every(uint64(everyXDays)).Day().Do(task)
+		s.Every(uint64(everyXDays)).Day().At(atTime).Do(task)
 	} else {
-		s.Every(uint64(everyXDays)).Days().Do(task)
+		s.Every(uint64(everyXDays)).Days().At(atTime).Do(task)
 	}
 	<-s.Start()
 }
